51986fb0ed0beaed2d8dc1c4660fd5d0944390f3
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2019-06-16 12:46:18 +0900

    6/16 정말밀림
    일단 내가 해야할 레이아웃은 마무리
    시간이 없다

---
+++ app/src/main/java/com/example/myapplication/AnnounceData.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+public class AnnounceData {
+    private String announce;
+    private String day;
+
+    public AnnounceData(String announce, String day) {
+        this.announce = announce;
+        this.day = day;
+    }
+
+    public String getAnnounce() {
+        return announce;
+    }
+
+    public void setAnnounce(String announce) {
+        this.announce = announce;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+}
